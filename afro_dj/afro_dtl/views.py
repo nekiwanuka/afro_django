9a2a1c8e3c4236c54b702243f862c82710422cad
NEKIWANUKA <nekiwanuka@gmail.com>
2023-04-12 09:08:33 +0300

    Adding tags to the index file.

--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    p_title ='Afro-Django'
-    return render(request, 'index.html', {'p_title':p_title})
+    p_title = 'Afro-Django'
+    username = 'Nich'
+    gender = 'Male'
+    return render(request,
+                  'index.html', 
+                  {'p_title':p_title, 'username':username, 'gender':gender})

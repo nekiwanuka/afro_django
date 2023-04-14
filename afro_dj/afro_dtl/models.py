2948a815e4b17b2704850773dc4cd9395e6eb1e0
NEKIWANUKA <nekiwanuka@gmail.com>
2023-04-14 17:06:56 +0300

    Adde login view, login url and login template

--- afro_dj/afro_dtl/models.py
+++ afro_dj/afro_dtl/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# we are creating a custom user model here named "user_account"
+class User_account(models.Model):
+    username = models.CharField(max_length=22)
+    gender = models.CharField(max_length=6)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=22)
+    signup_time = models.DateTimeField(auto_now_add=True)
--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -1,13 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
-    p_title = 'Afro-Django'
+    pr_title = 'Afro-Django'
     username = 'Nich'
-    gender = 'Male'
-    return render(request,
-                  'index.html', 
-                  {'p_title':p_title, 'username':username, 'gender':gender})
-    
+    gender = 'Female'
+    return render(
+        request,
+        'index.html', 
+        {'pr_title': pr_title, 'username':username, 'gender':gender}
+    )
+
 def register(request):
+
     return render(request, 'register.html')
+
+def registration(request):
+    user_name = request.POST['username']
+    email = request.POST['user_email']
+    password = request.POST['password']
+    gender = request.POST['gender']
+    user_details=[
+            user_name,email,password,gender
+        ]
+    print(user_details)
+    if User.objects.filter(username=user_name).first():
+        print('Username already exists.')
+        return render(request, 'login.html')
+    else:
+        user=User.objects.create_user(user_name, email, password)
+        return render(request, 'login.html')
+
+
+def login_user(request):
+    user_name = request.POST['username']
+    pwd = request.POST['password']
+    if User.objects.filter(username=user_name):
+        print("This username exists.")
+        logged_user = authenticate(request, username=user_name, password=pwd)
+        if logged_user is not None:
+            #here we are logging in the user
+            auth_login(request, logged_user)
+            print(user_name+" "+"Logged in successfuly")
+            return redirect('index')
+        else: 
+            #here we handle a scenario where the authentication has failed
+            return render(request, 'login.html')
+    else:
+        print("User Credentials do not exist.")
+        return render(request, 'login.html')
+    
+def login_page(request):
+    return render(request, 'login.html')
